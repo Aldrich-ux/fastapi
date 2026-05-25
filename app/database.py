01bed6a6444e11c9e4ef1101051ae8265da8cfd6
Aldrich-ux <qian10282021@163.com>
2026-05-25 19:33:24 +0800

    fix: remove unused psycopg

--- app/database.py
+++ app/database.py
@@ -1,9 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import psycopg  # noqa: F401
-from psycopg.rows import dict_row  # noqa: F401
-import time  # noqa: F401
 from .config import settings
 
 
@@ -19,29 +16,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-
-# while True:
-#     try:
-#         conn = psycopg.connect(
-#             conninfo=(
-#                 f"host={settings.database_hostname} "
-#                 f"dbname={settings.database_name} "
-#                 f"user={settings.database_username} "
-#                 f"password={settings.database_password} "
-#                 f"port={settings.database_port}"
-#             ),
-#             row_factory=dict_row)
-#         cursor = conn.cursor()
-#         print("Database connection was successful!")
-#         break
-#     except Exception as e:
-#         print("Database connection failed!")
-#         print("Error:", e)
-#         time.sleep(2)
